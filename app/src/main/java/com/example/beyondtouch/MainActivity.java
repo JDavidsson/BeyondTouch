c4b8da6ec5356986daef16a841b99e4d62c4b182
cmacfie <chris.macfie@gmail.com>
2017-05-03 19:04:51 +0200

    Started expeirmented with custom made layout

--- app/src/main/java/com/example/beyondtouch/MainActivity.java
+++ app/src/main/java/com/example/beyondtouch/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.beyondtouch;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    CustomFragment ff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment ff = new HomeFragment();
+        //Fragment ff = new HomeFragment();
+        ff = new CustomFragment();
+
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.container,ff).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, ff).commit();
         }
-
     }
+
 
 }
